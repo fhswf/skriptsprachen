38d7d82b42e0ccfae0fc88a0163bc0db3a9a8e50
Christian Gawron <gawron.christian@fh-swf.de>
2019-10-14 23:48:41 +0200

    examples for lecture three

---
+++ lecture_03/fibonacci.py
@@ -0,0 +1,12 @@
+""" Test module providing the fib() function"""
+
+def fib(n: int) -> int: 
+    """Calculate the nth Fibonacci number"""
+    if n < 2:
+        return [0, 1][n]
+    else:
+        return fib(n-1) + fib(n-2)  
+
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
